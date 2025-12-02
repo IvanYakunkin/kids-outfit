0ebeba7049069f32920896c7c0af83699d04b6c2
Ivan Yakunkin <ia.Yakunkin@gmail.com>
2025-12-03 00:21:06 +0300

    remove comments

--- server/src/modules/orders/dto/create-order.dto.ts
+++ server/src/modules/orders/dto/create-order.dto.ts
@@ -4,11 +4,6 @@ import { IsArray, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
 import { OrderProductsDto } from 'src/modules/order-products/dto/order-products.dto';
 
 export class CreateOrderDto {
-  // @ApiProperty({ description: 'ID Пользователя', type: Number, example: 1 })
-  // @IsNumber()
-  // @IsNotEmpty({ message: 'Пользователь не выбран' })
-  // userId: number;
-
   @ApiProperty({ description: 'Адрес доставки' })
   @IsString()
   @IsNotEmpty({ message: 'Адрес не может отсутствовать' })
