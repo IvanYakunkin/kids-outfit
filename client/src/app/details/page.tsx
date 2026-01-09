f9b52cf87fc04fa58d2912cb66c438a592f1d022
Ivan Yakunkin <ia.Yakunkin@gmail.com>
2026-01-09 09:52:25 +0300

    feat: details page

---
+++ client/src/app/details/page.tsx
@@ -0,0 +1,55 @@
+import styles from "./page.module.css";
+
+export default function details() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.title}>Информационный блок</div>
+      <div className={styles.details}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non
+          sodales elit, nec accumsan nisi. Curabitur finibus dolor in imperdiet
+          feugiat. Ut nec venenatis massa, gravida posuere nisl. Suspendisse
+          potenti. Donec ornare vestibulum sollicitudin. Morbi vulputate cursus
+          ante ac dictum. Aliquam sit amet ullamcorper tellus. Phasellus non
+          nibh orci. Vestibulum ac cursus mi, at consectetur elit. Suspendisse
+          potenti. Aliquam erat volutpat. Vivamus id vehicula ligula. Donec ac
+          aliquet justo. Nulla facilisi. Fusce non gravida arcu. Nunc rhoncus
+          ante in nisl ultrices, nec suscipit mi pharetra. Vivamus tincidunt
+          dolor nulla, at blandit mauris vulputate a.
+        </p>
+        <p>
+          Praesent sapien libero, lacinia quis ullamcorper eget, faucibus in ex.
+          Morbi sit amet nunc quis libero gravida eleifend id et lectus.
+          Interdum et malesuada fames ac ante ipsum primis in faucibus.
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+          posuere cubilia curae; Donec elementum augue pellentesque lacus
+          porttitor consectetur. Nam non purus nunc. Mauris eu ornare libero,
+          viverra lacinia mauris. Sed erat tellus, aliquam in velit id, euismod
+          maximus arcu. Nullam eget tempor purus. Sed lobortis nulla enim, non
+          maximus ligula porttitor at. Cras gravida sapien at congue
+          scelerisque. Aliquam erat volutpat. Vivamus suscipit accumsan nulla
+          pulvinar efficitur. Pellentesque velit ipsum, viverra sit amet nisi
+          vel, facilisis congue dui. Nunc imperdiet sagittis ex ac aliquam.
+          Integer luctus tincidunt dignissim.{" "}
+        </p>
+        <p>
+          Pellentesque habitant morbi tristique senectus et netus et malesuada
+          fames ac turpis egestas. Donec tempor diam quis placerat ullamcorper.
+          Donec laoreet ipsum mattis, rutrum magna in, pharetra lectus. Praesent
+          vel ipsum odio. Donec placerat nulla at nisi blandit, eu eleifend
+          augue sodales. Nulla libero eros, venenatis viverra eros vel, tempus
+          blandit lacus. Cras condimentum leo vitae sem euismod, et imperdiet
+          quam tempus. Vestibulum ante ipsum primis in faucibus orci luctus et
+          ultrices posuere cubilia curae; Vivamus nunc odio, maximus nec metus
+          at, laoreet scelerisque mi. Nunc dapibus a neque id congue. Ut
+          bibendum mauris eu justo blandit, non viverra orci rutrum. Proin et
+          arcu id felis efficitur faucibus non vitae nibh. Praesent cursus est
+          orci, nec gravida nunc varius eu. In hac habitasse platea dictumst. Ut
+          mollis elit nec ornare fringilla. Integer ut mi vel sapien blandit
+          blandit. Quisque arcu metus, congue ac malesuada fermentum, tristique
+          vel augue. Mauris pellentesque odio nec massa fringilla luctus.
+        </p>
+      </div>
+    </main>
+  );
+}
